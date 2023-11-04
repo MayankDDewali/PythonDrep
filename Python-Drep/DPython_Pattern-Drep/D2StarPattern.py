41638dfc08cea37e10404075baae4065a80a09b3
MayankDDewali <mayankddewali@gmail.com>
2023-11-04 22:40:07 +0530

    Third commit

--- Python-Drep/DPython_Pattern-Drep/D2StarPattern.py
+++ Python-Drep/DPython_Pattern-Drep/D2StarPattern.py
@@ -3,7 +3,7 @@ n = int(input("Enter the number of rows you want: "))
 
 for i in range(0,n+1):              #For making the rows in ascending order
 
-    for j in range(0,n-i+1):        #For making spaces
+    for j in range(0,n-i):        #For making spaces
         print(" ",end="")
         
     for k in range(1,2*i):          #For making stars
---
+++ Python-Drep/DPython_Pattern-Drep/D4StarPattern.py
@@ -0,0 +1,22 @@
+#For getting the number of rows
+n = int(input("Enter the number of rows you want: "))
+
+for i in range(0,n+1):              #For making the rows in ascending order
+
+    for j in range(0,n-i+1):        #For making spaces
+        print(" ",end="")
+        
+    for k in range(1,2*i):          #For making stars
+        print("*",end="")
+        
+    print("")                       #For changing the rows
+
+for i in range(n, 0, -1):               #For making the rows in descending order
+
+    for j in range(n-i,0,-1):                   #For making the spaces
+        print(" ", end="")
+        
+    for k in range(2*i-1, 0, -1):                   #For making the stars
+        print("*", end="")
+        
+    print("")                           #For changing the rows
