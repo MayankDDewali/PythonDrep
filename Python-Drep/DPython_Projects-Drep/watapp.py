b2ec49162623a7cd9d78b374f7417056c7d40cfa
MayankDDewali <mayankddewali@gmail.com>
2023-11-04 21:37:25 +0530

    Zero's commit

---
+++ Python-Drep/DPython_Projects-Drep/watapp.py
@@ -0,0 +1,10 @@
+import pyautogui as p
+import time
+limit = input("Enter the limit: ")
+i = 0
+message = input("Enter your message: ")
+time.sleep(5)
+while i<int(limit):
+    p.typewrite(message)
+    p.press("Enter")
+    i+=1
