a0ff9d05b737ccbf0c32ddaffbd0f6ffa379874a
MayankDDewali <mayankddewali@gmail.com>
2023-11-07 16:08:37 +0530

    Initial commit

--- Programiz(100qs)/Add2Numbers.py
+++
@@ -1,8 +0,0 @@
-#For getting first number
-a = int(input("Enter the value of first number: "))
-
-#For getting second number
-b = int(input("Enter the value of second number: "))
-
-#For adding these two numbers
-print(a+b)
---
+++ ProgramizQuestions/AddTwoNumbers.py
@@ -0,0 +1,8 @@
+#For getting the first number
+a = int(input("Enter the first value: "))
+
+#For getting the second number
+b = int(input("Enter the second value"))
+
+#For printing the sum
+print(a+b)
