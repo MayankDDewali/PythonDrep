6bd8417dfdc57ab0b206643fd5f8ae900acd3c28
MayankDDewali <mayankddewali@gmail.com>
2024-01-24 23:02:36 +0530

    Adding new codes

--- ProgramizQuestions/Pz23NumDivisibleByAnother.py
+++ ProgramizQuestions/Pz23NumDivisibleByAnother.py
@@ -2,4 +2,18 @@
 
 li = []
 
-range = int
+limit = int(input("How many numbers would you like to include: "))
+
+for i in range(limit):
+    num = int(input("Enter the {} number: ".format(i)))
+    li.append(num)
+
+x = int(input("Please enter the number you'd like to check for divisibility: "))
+
+li2 = []
+for i in li:
+    if(i%x==0):
+        li2.append(i)
+    else:
+        continue
+print("Numbers divisible by",x," are ",li2)
