c613a0579c3c299359d4272f18881e6deda1e3e8
MayankDDewali <mayankddewali@gmail.com>
2024-01-29 00:29:53 +0530

    Your commit message here

--- AI/JarvisD2/main.py
+++ AI/JarvisD2/main.py
@@ -5,7 +5,6 @@ import webbrowser
 import datetime
 from AppOpener import open
 import wikipedia
-import openai
 
 # Function for voice output
 def speak(text):
@@ -18,7 +17,7 @@ def takeCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         # Adjust the pause_threshold if needed
-        # r.pause_threshold = 
+        r.pause_threshold = 1
         print("Listening...")
         audio = r.listen(source)
         try:
@@ -31,16 +30,16 @@ def takeCommand():
 
 # Introduction
 # speak("Hello sir! What's your command?")
+speak(f"What's your command, sir?")
 
 while True:
     print("What's your command, sir?")
-    speak(f"What's your command, sir?")
     text = takeCommand()
-    
+
     # This is for opening websites
     sites = [["youtube", "https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"],
              ["google", "https://www.google.com"], ["github", "https://github.com/"]]
-    
+
     # Opening the site in the list of lists
     for site in sites:
         if f"Open {site[0]}".lower() in text.lower():
@@ -55,14 +54,13 @@ while True:
     # For opening applications
     apps = [["vs code", "Visual Studio Code"], ["whatsapp", "Whatsapp"], ["Dev C", "Dev-C++"],
             ["pycharm", "PyCharm Community Edition 2023.1.2"]]
-    
+
     # Opening the app in the list of lists
     for app in apps:
         if f"Open {app[0]}".lower() in text.lower():
             speak(f"Opening {app[0]}, sir...")
             open(app[1])
 
-    # For asking questions and retrieving answers from Wikipedia
     # For asking questions and retrieving answers from Wikipedia
     if "I have some questions".lower() in text.lower():
         speak("Alright! What's your question, sir...")
@@ -84,3 +82,10 @@ while True:
         except Exception as e:
             print(f"An error occurred: {e}")
             speak("Sorry, an error occurred while fetching information.")
+
+    # Adding extra questions
+    if "Tell me about the founder of".lower() in text.lower():
+        # speak("Of course")
+        qqs = "Raj Bhatt, Founder of Elara Capital, a global investment bank, brings over three decades of expertise in executing innovative financial transactions. As a financial re-engineering and fundraising expert, he has successfully managed complex deals across seven locations on three continents. Raj Bhatt's stellar track record includes achievements in restructuring, mergers, acquisitions, and raising equity and debt in international markets. Since founding Elara in 2002, he serves as its Vice-Chairman & CEO, guiding its evolution into a capital markets powerhouse with diverse ventures in corporate advisory, asset management, broking, mergers & acquisitions, and private equity, spanning offices in India, New York, Singapore, Mauritius, and London."
+        print(qqs)
+        speak(f"{qqs}")
