9df8b88f4e6b1a5eff0b88a9c68c8353fbc8af61
MayankDDewali <mayankddewali@gmail.com>
2024-01-29 00:37:42 +0530

    Adding codes

--- ProgramizQuestions/Pz26HCF.py
+++ ProgramizQuestions/Pz26HCF.py
@@ -1,20 +1,22 @@
 # Pz26- Python Program to Find HCF or GCD
 
-x = int(input("Enter the first number: "))
-y = int(input("Enter the second number: "))
+x = int(input("Enter the first number: "))          # For getting the first number
+y = int(input("Enter the second number: "))         # For getting the second number
 
-if(x==y):
+if(x==y):                                           # Checking wether the numbers are equal
     print("HCF of",x,"and",y,"is ",x)
-else:
+else:                                               # Condition for getting the smallest num among those
     if(x>y):
         xm = y
     else:
         xm = x
 
-    for i in range(1, xm):
+    for i in range(1, xm):                          # Getting the biggest number that can devide both num
         if(x%i==0 and y%i==0):
             hcf = i
         else:
             continue
 
     print("HCF of",x,"and",y,"is ",hcf)
+    
+# Made by MayankDd
